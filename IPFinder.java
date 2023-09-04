83c587c934a1e7d70f344705d2588d0ae6d0c840
maharajan1502 <129009154+maharajan1502@users.noreply.github.com>
2023-09-04 15:21:39 +0530

    comment adding section

--- IPFinder.java
+++ IPFinder.java
@@ -1,38 +1,43 @@
-import javax.swing.*;  
-import java.awt.event.*;  
-import java.net.*;  
-public class IPFinder extends JFrame implements ActionListener{  
-    JLabel l;  
-    JTextField tf;  
-    JButton b;  
-IPFinder(){  
-    super("IP Finder Tool");  
-    l=new JLabel("Enter URL:");  
-    l.setBounds(50,70,150,20);;  
-    tf=new JTextField();  
-    tf.setBounds(50,100,200,20);  
-      
-    b=new JButton("Find IP");  
-    b.setBounds(50,150,80,30);  
-    b.addActionListener(this);  
-    add(l);  
-    add(tf);  
-    add(b);  
-    setSize(300,300);  
-    setLayout(null);  
-    setVisible(true);  
-}  
-public void actionPerformed(ActionEvent e){  
-    String url=tf.getText();  
-    try {  
-        InetAddress ia=InetAddress.getByName(url);  
-        String ip=ia.getHostAddress();  
-        JOptionPane.showMessageDialog(this,ip);  
-    } catch (UnknownHostException e1) {  
-        JOptionPane.showMessageDialog(this,e1.toString());  
-    }  
-}  
-public static void main(String[] args) {  
-    new IPFinder();  
-}  
-}  
+import javax.swing.*;
+import java.awt.event.*;
+import java.net.*;
+
+public class IPFinder extends JFrame implements ActionListener {
+    JLabel l;
+    JTextField tf;
+    JButton b;
+
+    IPFinder() {
+        super("IP Finder Tool");
+        l = new JLabel("Enter URL:"); // url generator
+        l.setBounds(50, 70, 150, 20);
+        ;
+        tf = new JTextField();
+        tf.setBounds(50, 100, 200, 20);
+
+        b = new JButton("Find IP"); // buttto generator
+        b.setBounds(50, 150, 80, 30);
+        b.addActionListener(this);
+        add(l);
+        add(tf);
+        add(b);
+        setSize(300, 300);
+        setLayout(null);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String url = tf.getText();
+        try {
+            InetAddress ia = InetAddress.getByName(url); // exception handling
+            String ip = ia.getHostAddress();
+            JOptionPane.showMessageDialog(this, ip);
+        } catch (UnknownHostException e1) {
+            JOptionPane.showMessageDialog(this, e1.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        new IPFinder(); // MAIN FUNCTION
+    }
+}
